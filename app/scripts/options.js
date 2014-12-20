93d54e6b681e6d0b0346eef9b971b19aefa3162c
王佳裕 <fakefish.wjy@gmail.com>
2014-12-20 14:58:51 +0800

    no message

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -3,23 +3,19 @@
 console.log('\'Allo \'Allo! Option');
 
 chrome.runtime.onConnect.addListener(function(port) {
-  console.assert(port.name == "敲门");
+  console.assert(port.name == "WeShoot");
   port.onMessage.addListener(function(msg) {
-    if (msg.joke == "敲门")
-      port.postMessage({question: "是谁？"});
-    else if (msg.answer == "女士")
-      port.postMessage({question: "哪位女士？"});
-    else if (msg.answer == "Bovary 女士")
-      port.postMessage({question: "我没听清楚。"});
+    console.log(msg)
   });
 });
 
-var danmus = [1,2,3];
+var danmus = [1111,22222222,33333333];
 var HEIGHT = window.innerHeight;
 var danmuTop,danmuRight,danmuWidth,$danmuItem;
-$.each(danmus, function(danmu) {
-	console.log(danmu);
+$.each(danmus, function(key, danmu) {
+	// console.log(danmu);
 	$danmuItem = $('<span>'+danmu+'</span>').addClass('weshoot-item').addClass('default');
+  console.log($danmuItem.width())
 	$danmuItem.css({
 		top: ~~(Math.random() * HEIGHT),
 		right: -$danmuItem.width()
