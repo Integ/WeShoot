94b1808cec68feaf4c1fd36e9422954da7f577ac
integ <integ@segmentfault.com>
2014-12-20 15:41:49 +0800

    message

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -2,17 +2,12 @@
 
 console.log('\'Allo \'Allo! Option');
 
-chrome.runtime.onConnect.addListener(function(port) {
-  console.assert(port.name == "敲门");
-  port.onMessage.addListener(function(msg) {
-    if (msg.joke == "敲门")
-      port.postMessage({question: "是谁？"});
-    else if (msg.answer == "女士")
-      port.postMessage({question: "哪位女士？"});
-    else if (msg.answer == "Bovary 女士")
-      port.postMessage({question: "我没听清楚。"});
-  });
-});
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(request);
+        sendResponse({status: "OK"});
+    }
+);
 
 var danmus = [1,2,3];
 var HEIGHT = window.innerHeight;
