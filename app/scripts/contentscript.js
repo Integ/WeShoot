c2bcc791bc4f375e8ec6955a2f799497e75ed2a3
integ <integ@segmentfault.com>
2014-12-20 17:57:41 +0800

    fixqq

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,6 +1,9 @@
 'use strict';
-
-console.log('\'Allo \'Allo! Content script');
+if(location.host === 'w.qq.com' ) {
+    var platform = 'qq';
+} else if(location.host === 'wx.qq.com') {
+    var platform = 'wx';
+}
 
 var msg = {
     id: '',
@@ -15,36 +18,55 @@ var msg = {
     }
 };
 
-
 setInterval(function() {
-    var id = $('.chatItem:last').attr('un');
-    var avatar = 'https://wx.qq.com' + $('.chatItemContent:last .avatar').attr('src') || '';
-    var name = $('.chatItemContent:last .avatar').attr('title') || '';
-    var text = $('.chatItemContent:last pre').text() || '';
-    var time = $('.chatItem:last .time').text().trim() || '';
-    if($('.chatItemContent:last pre img').length) {
-        $('.chatItemContent:last pre img').each(function() {
-            var src = $(this).attr('src');
-            $(this).attr('src', 'https://wx.qq.com' + src);
-        });
-        var text = $('.chatItemContent:last pre').html();
-    }
-    if($('.chatItemContent:last .img_wrap img').length) {
-        var img = 'https://wx.qq.com' + $('.chatItemContent:last .img_wrap img').attr('src') || '';
-    }
-    var newMsg = {
-        id: id,
-        user: {
-            name: name,
-            avatar: avatar
-        },
-        time: time,
-        msg: {
-            text: text,
-            image: img
+    if(platform === 'qq') {
+        if($('.chat_content:last img').length) { //微信发带表情的内容
+            var text = $('.chat_content:last').html();
+        } else {
+            var text = $('.chat_content:last').text();
         }
-    };
-    if(id !== msg.id) {
+        var img = '';
+        var newMsg = {
+            id: $('.chat_content_group:last').attr('_sender_uin'),
+            user: {
+                name: $('.chat_content_group:last .chat_nick').text(),
+                avatar: $('.chat_content_group:last .chat_content_avatar').attr('src')
+            },
+            time: $('.chatItem:last .time').text().trim(),
+            msg: {
+                text:  text,
+                image: img
+            }
+        };
+    } else if (platform === 'wx') {
+        if($('.chatItemContent:last pre img').length) { //微信发带表情的内容
+            $('.chatItemContent:last pre img').each(function() {
+                var src = $(this).attr('src');
+                $(this).attr('src', 'https://wx.qq.com' + src);
+            });
+            var text = $('.chatItemContent:last pre').html();
+        } else {
+            var text = $('.chatItemContent:last pre').text();
+        }
+        if($('.chatItemContent:last .img_wrap img').length) {   //微信发照片
+            var img = 'https://wx.qq.com' + $('.chatItemContent:last .img_wrap img').attr('src');
+        } else {
+            var img = '';
+        }
+        var newMsg = {
+            id: $('.chatItem:last').attr('un'),
+            user: {
+                name: $('.chatItemContent:last .avatar').attr('title'),
+                avatar: 'https://wx.qq.com' + $('.chatItemContent:last .avatar').attr('src')
+            },
+            time: $('.chatItem:last .time').text().trim(),
+            msg: {
+                text:  text,
+                image: img
+            }
+        };
+    }
+    if(newMsg.id !== msg.id) {
         console.log(newMsg);
         chrome.runtime.sendMessage(newMsg, function(response) {
               console.log(response);
